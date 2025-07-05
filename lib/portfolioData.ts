ee083aafa1d0db83b19bdd02db4d61e62e3657c3
helmisaid <saidkun219@gmail.com>
2025-07-05 13:39:23 +0700

    add: new portfolio

--- lib/portfolioData.ts
+++ lib/portfolioData.ts
@@ -1,65 +1,100 @@
 export const portfolioItems = [
-    {
-      slug: "peluncuran-website-go-orbit",
-      image: "/go-orbit.png",
-      category: "Pengembangan Web & Keamanan Siber",
-      title: " Pengembangan Web untuk Go-Orbit.",
-      date: "3 Mei, 2025",
-      author: "EVOP",
-      description:
-        "Pada 3 Mei 2025, EVOP menyelenggarakan pelatihan untuk tim Go-Orbit, berfokus pada pengembangan web dan keamanan siber. Peserta mempelajari cara membangun website yang responsif menggunakan framework modern seperti Next.js, sambil menerapkan praktik pengkodean aman untuk mencegah kerentanan seperti injeksi SQL dan cross-site scripting (XSS). Sesi ini mencakup latihan praktis untuk mengamankan aplikasi web dan meningkatkan pengalaman pengguna melalui desain yang optimal.",
-      highlights: [
-        "Membangun website responsif dengan Cromwell",
-        "Praktik pengkodean aman untuk mencegah XSS",
-        "Optimasi performa website untuk Go-Orbit",
-      ],
-    },
-    {
-      slug: "pengembangan-website-feb-2025",
-      image: "/febunair.png",
-      category: "Keamanan Siber & Pengembangan Web",
-      title: "Keamanan Siber & Pengembangan Web untuk Profil Perusahaan FEB UNAIR.",
-      date: "2 Februari, 2025",
-      author: "EVOP",
-      description:
-        "pada 2 Februari 2025 EVOP merancang pengembangan profil perusahaan FEB UNAIR.EVOP mengembangkan website profesional menggunakan Wordpress, dengan fokus pada keamanan siber seperti pengamanan API dan pengujian penetrasi dasar. Pengembangan ini juga mencakup strategi untuk mengoptimalkan website agar ramah pengguna dan tahan terhadap ancaman siber seperti serangan brute force.",
-      highlights: [
-        "Pengembangan website profil dengan Wordpress",
-        "Pengamanan API RESTful",
-        "Pengujian penetrasi untuk mengidentifikasi kerentanan",
-      ],
-    },
-    {
-      slug: "temuan-bug-critical-pembicara-2024",
-      image: "/eventspeaker.png",
-      category: "Keamanan Siber",
-      title:
-        "Anggota EVOP Menemukan Bug critical di Universitas Airlangga dan Menjadi Pembicara Acara.",
-      date: "20 Februari, 2024",
-      author: "EVOP",
-      description:
-        "Pada 20 Februari 2024, seorang anggota EVOP menemukan kerentanan critical di sistem Universitas Airlangga, yang mengarah pada undangan sebagai pembicara di acara universitas. Sesi ini membahas pentingnya peretasan etis dan pengungkapan yang bertanggung jawab, dengan fokus pada deteksi kerentanan cross-site scripting (XSS). Anggota EVOP berbagi strategi untuk memperkuat keamanan siber institusi melalui kolaborasi proaktif.",
-      highlights: [
-        "Penemuan kerentanan Broken Access Control Critical",
-        "Proses pengungkapan yang bertanggung jawab",
-        "Pembicaraan tentang praktik peretasan etis",
-      ],
-    },
-    {
-      slug: "kuliah-latihan-siber-2024",
-      image: "/lecturer.png",
-      category: "Keamanan Siber",
-      title:
-        "EVOP Memberikan Kuliah kepada UKM UNAIR tentang Cara Melindungi Website dalam Acara Latihan Siber.",
-      date: "5 Agustus, 2024",
-      author: "EVOP",
-      description:
-        "Pada 5 Agustus 2024, EVOP memberikan kuliah kepada UKM UNAIR dalam acara latihan siber, fokus pada perlindungan website. Sesi ini mencakup konfigurasi firewall aplikasi web, penerapan header keamanan seperti CSP dan HSTS, serta strategi untuk mencegah serangan DDoS dan brute force. Peserta berpartisipasi dalam simulasi serangan siber untuk mempraktikkan teknik pertahanan secara langsung.",
-      highlights: [
-        "Konfigurasi firewall aplikasi web",
-        "Penerapan header keamanan CSP dan HSTS",
-        "Penerapan keamanan pada Cpanel Hosting",
-        "Simulasi pertahanan terhadap serangan DDoS",
-      ],
-    },
-  ];
+  {
+    slug: "peluncuran-website-go-orbit",
+    image: "/go-orbit.png",
+    category: "Pengembangan Web & Keamanan Siber",
+    title: " Pengembangan Web untuk Go-Orbit.",
+    date: "3 Mei, 2025",
+    author: "EVOP",
+    description:
+      "Pada 3 Mei 2025, EVOP menyelenggarakan pelatihan untuk tim Go-Orbit, berfokus pada pengembangan web dan keamanan siber. Peserta mempelajari cara membangun website yang responsif menggunakan framework modern seperti Next.js, sambil menerapkan praktik pengkodean aman untuk mencegah kerentanan seperti injeksi SQL dan cross-site scripting (XSS). Sesi ini mencakup latihan praktis untuk mengamankan aplikasi web dan meningkatkan pengalaman pengguna melalui desain yang optimal.",
+    highlights: [
+      "Membangun website responsif dengan Cromwell",
+      "Praktik pengkodean aman untuk mencegah XSS",
+      "Optimasi performa website untuk Go-Orbit",
+    ],
+  },
+  {
+    slug: "pengembangan-website-feb-2025",
+    image: "/febunair.png",
+    category: "Keamanan Siber & Pengembangan Web",
+    title:
+      "Keamanan Siber & Pengembangan Web untuk Profil Perusahaan FEB UNAIR.",
+    date: "2 Februari, 2025",
+    author: "EVOP",
+    description:
+      "pada 2 Februari 2025 EVOP merancang pengembangan profil perusahaan FEB UNAIR.EVOP mengembangkan website profesional menggunakan Wordpress, dengan fokus pada keamanan siber seperti pengamanan API dan pengujian penetrasi dasar. Pengembangan ini juga mencakup strategi untuk mengoptimalkan website agar ramah pengguna dan tahan terhadap ancaman siber seperti serangan brute force.",
+    highlights: [
+      "Pengembangan website profil dengan Wordpress",
+      "Pengamanan API RESTful",
+      "Pengujian penetrasi untuk mengidentifikasi kerentanan",
+    ],
+  },
+  {
+    slug: "temuan-bug-critical-pembicara-2024",
+    image: "/eventspeaker.png",
+    category: "Keamanan Siber",
+    title:
+      "Anggota EVOP Menemukan Bug critical di Universitas Airlangga dan Menjadi Pembicara Acara.",
+    date: "20 Februari, 2024",
+    author: "EVOP",
+    description:
+      "Pada 20 Februari 2024, seorang anggota EVOP menemukan kerentanan critical di sistem Universitas Airlangga, yang mengarah pada undangan sebagai pembicara di acara universitas. Sesi ini membahas pentingnya peretasan etis dan pengungkapan yang bertanggung jawab, dengan fokus pada deteksi kerentanan cross-site scripting (XSS). Anggota EVOP berbagi strategi untuk memperkuat keamanan siber institusi melalui kolaborasi proaktif.",
+    highlights: [
+      "Penemuan kerentanan Broken Access Control Critical",
+      "Proses pengungkapan yang bertanggung jawab",
+      "Pembicaraan tentang praktik peretasan etis",
+    ],
+  },
+  {
+    slug: "kuliah-latihan-siber-2024",
+    image: "/lecturer.png",
+    category: "Keamanan Siber",
+    title:
+      "EVOP Memberikan Kuliah kepada UKM UNAIR tentang Cara Melindungi Website dalam Acara Latihan Siber.",
+    date: "5 Agustus, 2024",
+    author: "EVOP",
+    description:
+      "Pada 5 Agustus 2024, EVOP memberikan kuliah kepada UKM UNAIR dalam acara latihan siber, fokus pada perlindungan website. Sesi ini mencakup konfigurasi firewall aplikasi web, penerapan header keamanan seperti CSP dan HSTS, serta strategi untuk mencegah serangan DDoS dan brute force. Peserta berpartisipasi dalam simulasi serangan siber untuk mempraktikkan teknik pertahanan secara langsung.",
+    highlights: [
+      "Konfigurasi firewall aplikasi web",
+      "Penerapan header keamanan CSP dan HSTS",
+      "Penerapan keamanan pada Cpanel Hosting",
+      "Simulasi pertahanan terhadap serangan DDoS",
+    ],
+  },
+  {
+    slug: "pengembangan-website-ospekkit-unair",
+    image: "/ospekkit-unair.png",
+    category: "Pengembangan Web",
+    title: "Pengembangan Website Ecommerce untuk Ospekkit Unair",
+    date: "16 Mei, 2025",
+    author: "EVOP",
+    description:
+      "Pada 16 Mei 2025, EVOP sukses meluncurkan website e-commerce yang komprehensif untuk Ospekkit Unair. Platform ini dirancang khusus untuk memenuhi kebutuhan mahasiswa baru Universitas Airlangga dengan menyediakan berbagai perlengkapan esensial selama periode ospek. Kami berfokus pada pengembangan fitur yang intuitif dan pengalaman pengguna yang lancar, memastikan proses pembelian yang mudah dan aman bagi setiap mahasiswa.",
+    highlights: [
+      "Berhasil mengembangkan dan meluncurkan platform e-commerce yang lengkap untuk Ospekkit Unair, memfasilitasi kebutuhan perlengkapan ospek mahasiswa baru.",
+      "Merancang dan mengimplementasikan UX yang intuitif dan mobile-responsive, memastikan pengalaman belanja yang optimal di berbagai perangkat.",
+      "Mengintegrasikan sistem pembayaran yang aman dan beragam dengan Payment Gateway Tripay, meningkatkan kemudahan dan kepercayaan dalam setiap transaksi.",
+      "Melakukan optimasi performa website secara menyeluruh, menghasilkan kecepatan loading yang tinggi dan stabilitas platform yang prima.",
+    ],
+  },
+  {
+  slug: "pengembangan-website-ospekkit-kampus",
+  image: "/ospekkit-kampus.png",
+  category: "Pengembangan Web",
+  title: "Pengembangan Website E-commerce Multikampus untuk Ospekkit Kampus",
+  date: "16 Mei, 2025", 
+  author: "EVOP",
+  description:
+    "Pada 16 Mei 2025, EVOP berhasil meluncurkan website e-commerce Ospekkit Kampus. Platform ini dirancang khusus untuk memenuhi kebutuhan perlengkapan ospek bagi mahasiswa baru di delapan kampus besar di Indonesia. Dengan menggunakan WordPress dan WooCommerce, kami membangun solusi yang skalabel dan mudah dikelola, menyediakan pengalaman belanja yang nyaman dan terpusat untuk berbagai universitas.",
+  highlights: [
+    "Berhasil mengembangkan dan mengimplementasikan platform e-commerce berbasis WordPress dan WooCommerce untuk skala multikampus.",
+    "Mengakomodasi kebutuhan perlengkapan ospek untuk delapan universitas besar di Indonesia dalam satu platform terintegrasi.",
+    "Membangun arsitektur website yang fleksibel untuk pengelolaan produk spesifik per kampus dan kategori.",
+    "Merancang desain UI/UX yang user-friendly dan dapat beradaptasi untuk audiens dari berbagai latar belakang kampus.",
+    "Mengintegrasikan sistem pembayaran yang aman dan beragam, memastikan proses transaksi yang mulus bagi seluruh mahasiswa.",
+    "Melakukan optimasi performa dan keamanan website** secara menyeluruh untuk menunjang volume trafik yang tinggi dari berbagai kampus.",
+  ],
+},
+];
